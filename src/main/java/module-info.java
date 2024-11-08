b0df685ddebe157ef39dbda0febcabf20d1dc434
Julio <juliogtrenard@gmail.com>
2024-11-08 09:08:20 +0100

    ADD estructura del proyecto

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module es.juliogtrenard.examen_dein_julio_gonzalez {
 
     opens es.juliogtrenard.examen_dein_julio_gonzalez to javafx.fxml;
     exports es.juliogtrenard.examen_dein_julio_gonzalez;
+    exports es.juliogtrenard.examen_dein_julio_gonzalez.controladores;
+    opens es.juliogtrenard.examen_dein_julio_gonzalez.controladores to javafx.fxml;
 }
